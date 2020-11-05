8cbcb3f5c276c5732108f9c75f1c3fdc205af4bf
Andrei Tolstoi <tolstoi.a@gmail.com>
2020-11-05 10:44:02 +0200

    feat(RadioSelectionField): create RadioSelectionField component

--- src/elements/RadioSelectionField.tsx
+++ src/elements/RadioSelectionField.tsx
@@ -1,18 +1,32 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-type RadioSelectionFieldProps = {};
+type RadioSelectionFieldProps = {
+  label: string;
+  id: string;
+  name: string;
+  value: string;
+};
 type RadioSelectionFieldState = {};
 
 export class RadioSelectionField extends Component<
   RadioSelectionFieldProps,
   RadioSelectionFieldState
 > {
-  constructor(props: ConfiguratorProps) {
+  constructor(props: RadioSelectionFieldProps) {
     super(props);
   }
 
   render() {
-    return <div></div>;
+    const { label, id, name, value } = this.props;
+
+    return (
+      <div>
+        <label htmlFor={id}>{label}</label>
+        <input type='radio' id={id} name={name} value={value}>
+          {value}
+        </input>
+      </div>
+    );
   }
 }
