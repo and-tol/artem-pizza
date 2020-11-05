771cce63ade7a5e5d2923f2f1c8f609aa1bd9c68
Andrei Tolstoi <tolstoi.a@gmail.com>
2020-11-05 10:07:46 +0200

    init: start RadioSelectionField component

---
+++ src/elements/RadioSelectionField.tsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+type RadioSelectionFieldProps = {};
+type RadioSelectionFieldState = {};
+
+export class RadioSelectionField extends Component<
+  RadioSelectionFieldProps,
+  RadioSelectionFieldState
+> {
+  constructor(props: ConfiguratorProps) {
+    super(props);
+  }
+
+  render() {
+    return <div></div>;
+  }
+}
