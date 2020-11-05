b06655021f7430552cd2122216a44efb42033cb6
Andrei Tolstoi <tolstoi.a@gmail.com>
2020-11-05 16:14:53 +0200

    feat(availableData): create all available data for COnfigurator

--- src/availabelData.ts
+++ src/availabelData.ts
@@ -1,21 +1,48 @@
-type PizzaSize = { size: string; name: string };
-type PizzaDough = { dough: string; name: string };
-type PizzaSauce = { sauce: string };
+// type PizzaSize = { size: string; name: string };
+// type PizzaDough = { dough: string; name: string };
+// type PizzaSauce = { sauce: string,name:'string' };
+// type PizzaCheese = { cheese: string,name:'string' };
+// type PizzaVegetables= { vegetable: string,name:'string' };
+// type PizzaMeat = { meat: string, name: 'string' };
+
+type PizzaParams = {param: string,name:'string'}
 
 interface AvailabelData {
-  pizzaSize: PizzaSize[];
-  pizzaDough: PizzaDough[];
-  pizzaSauce: PizzaSauce[];
+  pizzaSize: PizzaParams[];
+  pizzaDough: PizzaParams[];
+  pizzaSauce: PizzaParams[];
+  pizzaCheese: PizzaParams[];
+  pizzaVegetables: PizzaParams[];
+  pizzaMeat: PizzaParams[];
 }
 
 export const availabelData: AvailabelData = Object.freeze({
-  pizzaSize: [
-    { size: '30', name: 'size30' },
-    { size: '35', name: 'size35' },
-  ],
-  pizzaDough: [
-    { dough: 'Тонкое', name: 'thinDough' },
-    { dough: 'Пышное', name: 'puffyDough' },
-  ],
-  pizzaSauce: [{ sauce: 'Томатный' }, { sauce: 'Белый' }, { sauce: 'Острый' }],
-});
+         pizzaSize: [
+           { param: '30', name: 'size30' },
+           { param: '35', name: 'size35' },
+         ],
+         pizzaDough: [
+           { param: 'Тонкое', name: 'thinDough' },
+           { param: 'Пышное', name: 'puffyDough' },
+         ],
+         pizzaSauce: [
+           { param: 'Томатный', name:'tomatoSauce'},
+           { param: 'Белый', name:'whiteSauce'},
+           { param: 'Острый', name:'spicySauce'},
+         ],
+         pizzaCheese: [
+           { param: 'Моцарелла', name:'Cheese'},
+           { param: 'Чеддер', name:'Cheese'},
+           { param: 'Дор Блю', name:'Cheese'},
+         ],
+         pizzaVegetables: [
+           { param: 'помидор', name:'tomatoVegetable'},
+           { param: 'грибы', name:'mushroomVegetable'},
+           { param: 'перец', name:'peperVegetable'},
+         ],
+         pizzaMeat: [
+           { param: 'бекон', name:'baconMeat'},
+           { param: 'пепперони', name:'pepperoniMeat'},
+           { param: 'бекон', name:'hamMeat'},
+         ],
+       });
