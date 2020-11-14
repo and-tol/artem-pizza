9be7781e3f6ae3a1b9856cd6b2274de03be8bb06
Andrei Tolstoi <tolstoi.a@gmail.com>
2020-11-14 16:19:42 +0200

    feat: add sector with sause & dough

--- src/App.tsx
+++ src/App.tsx
@@ -14,12 +14,38 @@ function App() {
         <fieldset>
           <legend>Размер</legend>
           <label>
-            <input type='radio' name='size' value='thin' />
+            <input type='radio' name='size' value='30' />
             30см
           </label>
           <label>
-            <input type='radio' name='size' value='Puffy' />
+            <input type='radio' name='size' value='35' />
             35см
+          </label>
+        </fieldset>
+        <fieldset>
+          <legend>Тесто</legend>
+          <label>
+            <input type='radio' name='size' value='thin' />
+            Тонкое
+          </label>
+          <label>
+            <input type='radio' name='size' value='puffy' />
+            Пышное
+          </label>
+        </fieldset>
+        <fieldset>
+          <legend>Соус</legend>
+          <label>
+            <input type='radio' name='size' value='tomato' />
+            Томатный
+          </label>
+          <label>
+            <input type='radio' name='size' value='white' />
+            Белый
+          </label>
+          <label>
+            <input type='radio' name='size' value='spicy' />
+            Острый
           </label>
         </fieldset>
       </form>
