c582c9949e226de141eaa4c647f555bcc3d72de8
Andrei Tolstoi <tolstoi.a@gmail.com>
2020-11-21 19:33:02 +0200

    feat: create tests for PizzaOrderPrewie

---
+++ src/PizzaOrderPreview.test.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { PizzaOrderPreview } from './PizzaOrderPreview';
+
+describe('PizzaOrderPreview', () => {
+  it('renders correctly', () => {
+    // Arrange
+    const { getByText } = render(
+      <PizzaOrderPreview
+        pizza={{
+          cheese: ['mozarella'],
+          dough: 'thin',
+          meat: ['bacon'],
+          sauce: 'tomato',
+          size: '30',
+          vegetables: ['tomato'],
+        }}
+      />
+    );
+    // Act & Assert
+    expect(getByText("Размер: 30см")).toBeInTheDocument()
+    expect(getByText('Тесто: тонкое')).toBeInTheDocument();
+    expect(getByText('Соус: томатный')).toBeInTheDocument();
+    expect(getByText('Сыр: моцарелла')).toBeInTheDocument();
+    expect(getByText('Овощи: помидор')).toBeInTheDocument();
+    expect(getByText('Мясо: бекон')).toBeInTheDocument();
+  });
+});
