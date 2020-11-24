d5509c1e7e6ac90e1537fae0698e7b884c151542
Andrei Tolstoi <tolstoi.a@gmail.com>
2020-11-24 20:31:45 +0200

    add old file

---
+++ temp/oldTypes.js
@@ -0,0 +1,30 @@
+// export interface SizeType {
+//   '30': PizzaDataWithPrice;
+//   '35': PizzaDataWithPrice;
+// }
+// export interface DoughType {
+//   thin: PizzaData;
+//   puffy: PizzaData;
+// }
+// export interface SauceType {
+//   tomato: PizzaData;
+//   white: PizzaData;
+//   spicy: PizzaData;
+// }
+// export interface CheeseType {
+//   mozarella: PizzaDataWithPrice;
+//   cheddar: PizzaDataWithPrice;
+//   dorblue: PizzaDataWithPrice;
+// }
+// export interface VegetablesType {
+//   tomato: PizzaDataWithPrice;
+//   mushrooms: PizzaDataWithPrice;
+//   pepper: PizzaDataWithPrice;
+//   pineapple: PizzaDataWithPrice;
+//   olives: PizzaDataWithPrice;
+//   onion: PizzaDataWithPrice;
+//   broccoli: PizzaDataWithPrice;
+//   bacon: PizzaDataWithPrice;
+//   pepperoni: PizzaDataWithPrice;
+//   ham: PizzaDataWithPrice;
+// }
