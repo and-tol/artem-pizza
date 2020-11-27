6091c825bfd722b4fb6687ea2372114e4c9cc335
Andrei Tolstoi <tolstoi.a@gmail.com>
2020-11-27 16:14:26 +0200

    feat: create not page 404, start book file

---
+++ src/navigation/book.ts
@@ -0,0 +1,29 @@
+// Pages
+
+
+export const book = Object.freeze({
+  root: {
+    url: '/',
+  },
+  pizzaConfigurator: {
+    url: '/configurator',
+  },
+  pizzaOrderPreview: {
+    url: '/order-preview',
+  },
+  checkout: {
+    url: '/checkout',
+  },
+  login: {
+    url: '/login',
+  },
+  orderList: {
+    url: '/order-list',
+  },
+  receipt: {
+    url: '/receipt',
+  },
+  notFound: {
+    page: ""
+  }
+});
