7153a5a66ec11dd0acbab129f905f74f3853d80f
Andrei Tolstoi <tolstoi.a@gmail.com>
2020-12-03 15:43:59 +0200

    feat: add hook-form to LoginPage without test

--- src/pages/login-page/LoginPage.tsx
+++ src/pages/login-page/LoginPage.tsx
@@ -1,22 +1,45 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+
+type Profile = {
+  login: string;
+  password: string;
+};
 
 export const LoginPage = () => {
+  const { register, handleSubmit, errors } = useForm<Profile>();
+
+  const onSubmit = handleSubmit(data => {
+    console.log(data);
+  });
+
   return (
     <>
-      <h1>Страница входа в приложение</h1>
-      <form>
+      <h1>Авторизация</h1>
+      <form onSubmit={onSubmit}>
         <fieldset>
           <label>
             Э-почта
-            <input type='text' name='login' />
+            <input
+              ref={register({ required: true })}
+              type='text'
+              name='login'
+            />
+            {errors.login && <div>Неправильное имя пользователя</div>}
           </label>
           <label>
             Пароль
-            <input type='password' name='password' />
+            <input
+              ref={register({ required: true })}
+              type='password'
+              name='password'
+            />
+            {errors.password && <div>Неправильный пароль</div>}
           </label>
+
         </fieldset>
-        <button>Авторизоваться</button>
+        <button>Войти</button>
         <Link to='/signup'>На страницу Регистрации </Link>
       </form>
     </>
