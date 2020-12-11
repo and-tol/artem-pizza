acc9f2c7a5bfe59f88477cef473b50e5b6a0288f
Andrei Tolstoi <tolstoi.a@gmail.com>
2020-12-11 10:25:00 +0200

    deleted

--- src/pages/orders-list-page/OrdersListPage.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export const OrdersListPage = () => {
-  return (
-    <>
-      <h1>Страница со списком заказов</h1>
-    </>
-  );
-};
--- src/pages/receipt-page/ReceiptPage.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export const ReceiptPage = () => {
-  return (
-    <>
-      <h1>Страница с чеком</h1>
-    </>
-  );
-};
