ed79644b68f740ac4b1a7bf40123056d507157c3
Andrei Tolstoi <tolstoi.a@gmail.com>
2021-02-10 17:36:16 +0200

    chore: minor change

---
+++ client/TODO.js
@@ -0,0 +1,25 @@
+*В CheckoutForm.test.tsx добавить тест компонента OrderPreview*
+
+
+*[rootReducer = combineReducer produces type 'Never' with typescript](https://stackoverflow.com/questions/62706721/rootreducer-combinereducer-produces-type-never-with-typescript)*
+
+*[Redux testing](https://redux.js.org/recipes/writing-tests)*
+
+
+// TODO: Написать PropTypes
+// RadioGroupSwitcher -> при клике меняется начертание шрифта на bold -> умеличивается ширина -> кнопки скачут
+
+// TODO: сделать задержку загрузки страницы
+// TODO: component Order: сделать пунктирную линию с помощью background repeat gradient
+
+// TODO: Checkout: сделать проверку года
+// TODO: Checkout: сделать проверку CVV
+// TODO: Checkout: сделать функцию очистки input
+// TODO: Checkout: показывать лого платёжной системы
+
+// TODO: реализовать функционал в приложении admin описанный здесь: [Загрузка файлов на CSS и JS с нуля. JavaScript практика](https://youtu.be/5vx2PVClSVU), https://react-hook-form.com/ru/advanced-usage#WizardFormFunnel
+
+// TODO: CheckoutForm расширить валидацию формы
+// FIXME: HeaderOrdersList: при :hover должен меняться цвет иконки
+// TODO: Orders List: сделать логику - доставка становится выполненой через час после отсылки на сервер
+
