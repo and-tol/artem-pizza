bf07ff0edb915514aba26335390deb562cfd9b35
Andrei Tolstoi <tolstoi.a@gmail.com>
2021-02-17 16:45:07 +0200

    feat: add sentry in client app

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import * as Sentry from '@sentry/react';
+import { Integrations } from '@sentry/tracing';
 // Components
 import App from './App';
 import {ErrorBoundary} from './ErrorBoundary'
@@ -9,6 +11,16 @@ import {ErrorBoundary} from './ErrorBoundary'
 import './index.css';
 
 import { store } from './init/store';
+
+Sentry.init({
+  dsn:
+    'https://ddae50184a5c43fb883e96e9fd7e550c@o526096.ingest.sentry.io/5641236',
+  integrations: [new Integrations.BrowserTracing()],
+
+  // We recommend adjusting this value in production, or using tracesSampler
+  // for finer control
+  tracesSampleRate: 1.0,
+});
 
 ReactDOM.render(
   <React.StrictMode>
